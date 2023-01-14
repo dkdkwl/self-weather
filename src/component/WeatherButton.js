ab40a40b070c390257a085a7c8582ecbae282b10
dkdkwl <dkdkwl@naver.com>
2023-01-14 14:40:04 +0900

    두번째 커밋

--- src/component/WeatherButton.js
+++ src/component/WeatherButton.js
@@ -2,7 +2,12 @@ import React from 'react'
 
 const WeatherButton = () => {
   return (
-    <div>WeatherButton</div>
+    <ul className='buttonList'>
+        <li className='buttonList--item'><a href="javascript:void(0);">서울</a></li>
+        <li className='buttonList--item'><a href="javascript:void(0);">일본</a></li>
+        <li className='buttonList--item'><a href="javascript:void(0);">인도</a></li>
+        <li className='buttonList--item'><a href="javascript:void(0);">미국</a></li>
+    </ul>
   )
 }
 
