0493c50fc73682186a415a97452faf880658f914
dkdkwl <dkdkwl@naver.com>
2023-01-14 14:26:10 +0900

    첫번째 커밋

---
+++ src/component/WeatherBox.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const WeatherBox = () => {
+  return (
+    <div>
+        <h4>서울</h4>
+        <p>15도씨</p>
+        <p>맑음</p>
+    </div>
+  )
+}
+
+export default WeatherBox
