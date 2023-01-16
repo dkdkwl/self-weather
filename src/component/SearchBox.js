86934e6533319f85381a841e5d4d9e1a56f866cc
dkdkwl <dkdkwl@naver.com>
2023-01-16 14:58:30 +0900

    검색 필터 추가

--- src/component/SearchBox.js
+++ src/component/SearchBox.js
@@ -1,18 +1,29 @@
 import React,{useState} from 'react';
 import { countryList } from '../recoil/country';
+import { cityState } from '../recoil/atom';
+import { useRecoilState } from 'recoil';
 
 const SearchBox = () => {
-  const test = countryList ;
-  console.log(test);
+  const countryData = countryList ;
   const [searchTerm, setSearchTerm] = useState("");
+  const searched = countryData.filter((item) =>
+    item.toLowerCase().includes(searchTerm)
+  );
+
+  let [getCity,setGetCity] = useRecoilState(cityState);
+  console.log(getCity)
   
 
   return (
     <div className="input-group">
-        <input type="search" placeholder="도시 이름 입력"
-        onChange={ (e)=>{ setSearchTerm(e.target.value); }}/>
-        <button></button>
-{}
+        <div className='inputArea'>
+          <input type="search" placeholder="도시 이름 입력"
+          onChange={(e)=>{ setSearchTerm(e.target.value.toLowerCase()); }}/>  
+          <button>검색</button>
+        </div>
+        <div className='outArea'>
+          {searched.map((item,i) => ( <div key={i}>{item}</div> ))}
+        </div>
     </div>
   )
 }
